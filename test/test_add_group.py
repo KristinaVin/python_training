d74834932db4c3d78e27f286ccbe90bab2212853
Kristina <ladykristy@mail.ru>
2019-02-02 18:37:29 +0600

    проверка на длину списка для групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,10 +3,13 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="dsvv", header="sdvsdv", footer="svdsv"))
-
-
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 def test_add_empty_group(app):
-     app.group.create(Group(name="", header="", footer=""))
-
+    old_groups = app.group.get_group_list()
+    app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
