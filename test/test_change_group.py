74f17daac7abcdf80cdeaf1661dbeee54e874371
Kristina <ladykristy@mail.ru>
2019-02-03 12:48:17 +0600

    кеширование для всех тестов групп

--- test/test_change_group.py
+++
@@ -1,7 +0,0 @@
-from model.group import Group
-
-
-def test_change_first_group(app):
-    if app.group.count() == 0:
-        app.group.create(Group(name="test"))
-    app.group.change_first_group(Group(name="fff", header="ffff", footer="ffff"))
