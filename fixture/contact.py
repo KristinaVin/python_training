73c180f0315642cf06194d67d0d050c8af9ef4c9
Kristina <ladykristy@mail.ru>
2019-02-10 19:04:36 +0600

    работа со строками в контактах

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.support.ui import Select
 from model.contact import Contact
+import re
 
 class ContactHelper:
 
@@ -49,20 +50,31 @@ class ContactHelper:
     def  modify_contact_by_index(self, index, contact):
         self.app.return_to_home_page()
         wd = self.app.wd
-        self.select_pencil_by_index(index)
+        self.open_contact_to_edit_by_index(index)
         #  submit modification
         self.contract_form_fill(contact)
         wd.find_element_by_name("update").click()
         self.app.return_to_home_page()
         self.contact_cache = None
 
+    def open_contact_view_by_index (self,index):
+        wd = self.app.wd
+        self.app.open_home_page()
+        row= wd.find_elements_by_name("entry")[index]
+        cell=row.find_elements_by_tag_name("td")[6]
+        cell.find_element_by_tag_name("a").click()
+
+
     def select_contact_by_index(self, index):
         wd = self.app.wd
         wd.find_elements_by_name("selected[]")[index].click()
 
-    def select_pencil_by_index(self, index):
+    def open_contact_to_edit_by_index(self, index):
         wd = self.app.wd
-        wd.find_elements_by_xpath("//img[@alt='Edit']")[index].click()
+        self.app.open_home_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[7]
+        cell.find_element_by_tag_name("a").click()
 
     def select_first_contact(self):
         wd = self.app.wd
@@ -154,15 +166,42 @@ class ContactHelper:
     def get_contact_list(self):
         if self.contact_cache is None:
             wd = self.app.wd
-            self.app.return_to_home_page()
+            self.app.open_home_page()
             self.contact_cache = []
-            for element in wd.find_elements_by_css_selector('tr[name="entry"]'):
-                id = element.find_element_by_name("selected[]").get_attribute("id")
-                last_name = element.find_elements_by_css_selector("td")[1].text
-                first_name = element.find_elements_by_css_selector("td")[2].text
-                self.contact_cache.append(Contact(id=id, firstname=first_name, lastname=last_name))
+            for row in wd.find_elements_by_name("entry"):
+                cells = row.find_elements_by_tag_name("td")
+                firstname = cells[2].text
+                lastname = cells[1].text
+                id = cells[0].find_element_by_tag_name("input").get_attribute("value")
+                all_phones = cells[5].text
+                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id,
+                                                  all_phones_from_home_page = all_phones))
         return list(self.contact_cache)
 
 
 
+    def get_contact_info_from_edit_page(self, index):
+        wd = self.app.wd
+        self.open_contact_to_edit_by_index(index)
+        firstname = wd.find_element_by_name("firstname").get_attribute("value")
+        lastname = wd.find_element_by_name("lastname").get_attribute("value")
+        id = wd.find_element_by_name("id").get_attribute("value")
+        home_telephone = wd.find_element_by_name("home").get_attribute("value")
+        work_telephone = wd.find_element_by_name("work").get_attribute("value")
+        mobile_telephone = wd.find_element_by_name("mobile").get_attribute("value")
+        phone2 = wd.find_element_by_name("phone2").get_attribute("value")
+        return Contact(firstname=firstname, lastname=lastname, id=id,
+                       home_telephone=home_telephone, work_telephone=work_telephone,
+                       mobile_telephone=mobile_telephone, phone2=phone2)
+
+    def get_contact_from_view_page(self, index):
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        text = wd.find_element_by_id("content").text
+        home_telephone = re.search("H: (.*)", text).group(1)
+        work_telephone = re.search("W: (.*)", text).group(1)
+        mobile_telephone = re.search("M: (.*)", text).group(1)
+        phone2 = re.search("P: (.*)", text).group(1)
+        return Contact(home_telephone=home_telephone, work_telephone=work_telephone,
+                       mobile_telephone=mobile_telephone, phone2=phone2)
 
--- model/contact.py
+++ model/contact.py
@@ -2,7 +2,9 @@ from sys import maxsize
 
 class Contact:
 
-    def __init__(self, firstname=None, middlename=None, lastname=None, nickname=None, title=None, company=None, address=None, home_telephone=None,
+    def __init__(self, firstname=None, middlename=None, lastname=None, nickname=None, title=None, company=None, address=None,
+                 all_phones_from_home_page = None,
+                 home_telephone=None,
                        mobile_telephone=None, work_telephone=None, fax=None, email=None, email2=None, email3=None,  homepage=None, bday=None, bmonth=None, byear=None, aday=None, amonth=None, ayear=None, notes=None,
                        address2=None, phone2=None, id=None):
         self.firstname = firstname
@@ -30,12 +32,13 @@ class Contact:
         self.address2 = address2
         self.phone2 = phone2
         self.id = id
+        self.all_phones_from_home_page = all_phones_from_home_page
 
     def __repr__(self):
         return "%s:%s:%s" % (self.id, self.firstname, self.lastname)
 
     def __eq__(self, other):
-        return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname and self.lastname == other.lastname
+        return (self.id is None or other.id is None or self.id == other.id) and (self.firstname is None or other.firstname is None or self.firstname == other.firstname) and (self.lastname is None or other.lastname is None or self.lastname == other.lastname)
 
     def id_or_max(self):
         if self.id:
