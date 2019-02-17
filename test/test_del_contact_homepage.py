00d32075a3086190c22d35b2b0c3bbb85f570bba
Kristina <ladykristy@mail.ru>
2019-02-17 10:49:49 +0600

    задание 19

--- test/test_del_contact_homepage.py
+++ test/test_del_contact_homepage.py
@@ -1,33 +1,9 @@
 from model.contact import Contact
 from random import randrange
-import pytest
-import random
-import string
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-months = ["January", "February", "March", "April", "May", "June", "July",
-          "August", "September", "October", "November", "December"]
-
-testdata = [Contact(firstname=random_string("first_name", 10), middlename=random_string("middle_name", 10),
-                    lastname=random_string("last_name", 10), nickname=random_string("nickname", 10),
-                     title=random_string("title", 10),
-                    company=random_string("company", 10), address=random_string("address", 10),
-                    home_telephone=random_string("home_phone", 10), mobile_telephone=random_string("mobile_phone", 10),
-                    work_telephone=random_string("work_phone", 10), fax=random_string("fax", 10),
-                    email=random_string("email_1", 10), email2=random_string("email_2", 10),
-                    email3=random_string("email_3", 10), homepage=random_string("homepage", 10),
-                    bday=str(random.randrange(1, 32)),  bmonth=random.choice(months), aday=str(random.randrange(1, 32)),
-                    amonth=random.choice(months), byear=random_string("b_year", 10), ayear=random_string("a_year", 10),
-                    address2=random_string("address_2", 10), phone2=random_string("phone_2", 10),
-                    notes=random_string("notes", 10))
-            for i in range(5)]
 
 
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_del_contact(app, contact):
+def test_del_contact(app, json_contacts):
+    contact=json_contacts
     if app.contact.count() == 0:
         app.contact.create_contact(contact)
     old_contact = app.contact.get_contact_list()
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,19 +1,10 @@
 from model.group import Group
 from random import randrange
-import pytest
-import random
-import string
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-testdata = [Group(name="", header="", footer="")] + \
-           [Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
-            for i in range(2)]
 
-@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
-def test_modify_group_name(app, group):
+def test_modify_group_name(app, json_groups):
+    group = json_groups
     if app.group.count() == 0:
         app.group.create(group)
     old_groups = app.group.get_group_list()
@@ -26,8 +17,9 @@ def test_modify_group_name(app, group):
     old_groups[index]=group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
-@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
-def test_modify_group_header(app, group):
+
+def test_modify_group_header(app, json_groups):
+    group = json_groups
     if app.group.count() == 0:
         app.group.create(group)
     old_groups = app.group.get_group_list()
